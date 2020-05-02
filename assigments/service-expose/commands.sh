80a5414212c9fb30e97d7db3c6e3074c6feb4a7f
Eduardo Thums <eduardocristiano01@gmail.com>
2020-05-01 22:45:01 -0300

    ✨ Add base service expose assigment

---
+++ assigments/service-expose/commands.sh
@@ -0,0 +1,17 @@
+# ClusterIP
+kubectl create deploy elasticsearch --image elasticsearch:2
+kubectl create deploy nginx --image nginx
+
+kubectl expose deploy/nginx --port 80
+
+kubectl exec -it $ELASTICSEARCH_POD_NAME -- bash
+
+curl http://nginx:80
+
+# NodePort
+kubectl create deploy elasticsearch --image elasticsearch:2
+kubectl create deploy nginx --image nginx
+
+kubectl expose deploy/nginx --port 80 --type NodePort
+
+curl http://$NODE_IP:$EXPODE_SERVICE_PORT
