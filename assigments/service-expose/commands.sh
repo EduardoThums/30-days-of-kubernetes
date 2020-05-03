36243a9be1c1dc3984e2e31c6f35e0cb57904b0b
Eduardo Thums <eduardocristiano01@gmail.com>
2020-05-02 21:36:05 -0300

    ✨ Change service assigment to add curl with cluster ip

--- assigments/service-expose/commands.sh
+++ assigments/service-expose/commands.sh
@@ -7,6 +7,7 @@ kubectl expose deploy/nginx --port 80
 kubectl exec -it $ELASTICSEARCH_POD_NAME -- bash
 
 curl http://nginx:80
+curl http://$SERVICE_CLUSTER_IP:80
 
 # NodePort
 kubectl create deploy elasticsearch --image elasticsearch:2
